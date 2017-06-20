6a6b6b6f6028cf9a38258bf1209486369b9d1e24
Thomas Phillips <thomasphillips3@gmail.com>
2017-06-20 13:58:30 -0700

    Get parameters from user instead of hard-coding.

--- hello.cpp
+++ hello.cpp
@@ -1,19 +1,35 @@
 // hello.cpp
 // Author: Thomas Phillips
-// Description: Print "Hello World!" 4 times on a line for 6 lines, where each is printed in a field of 17 spaces.
+// Description: Prompt the user for a phrase, and number of times to repeat.
+// Get the phrase, number of lines, and number of times to repeat from the user.
 
 #include <iostream>
 using namespace std;
 
 int main() {
-  const int WIDTH = 17;
-  const int LINES = 6;
-  const int TIMES = 4;
+  int width, lines, times;
+  string phrase = "Hello World!";
+  string prompt1 = "Enter a phrase: ";
+  string prompt2 = "Number of lines: ";
+  string prompt3 = "Times to repeat: ";
+  string prompt4 = "String width: ";
 
-  for (int i=0; i<LINES; i++) {
-    for (int j=0; j<TIMES; j++) {
-      cout.width(WIDTH);
-      cout << "Hello World!";
+  cout << prompt1;
+  getline(cin, phrase);
+
+  cout << prompt2;
+  cin >> lines;
+
+  cout << prompt3;
+  cin >> times;
+
+  cout << prompt4;
+  cin >> width;
+
+  for (int i=0; i<lines; i++) {
+    for (int j=0; j<times; j++) {
+      cout.width(width);
+      cout << phrase;
     }
     cout << endl;
   }

00615ee21b9e1de1ad704dc032b80ef869e5015b
Thomas Phillips <thomasphillips3@gmail.com>
2017-06-21 21:36:51 -0700

    Examples 3 and 4
    - Example 3: Times Table
    - Example 4: Guessing Game

---
+++ example3.cpp
@@ -0,0 +1,19 @@
+// https://developers.google.com/edu/c++/getting-started
+// Example 3: What does this program output?
+// Time tables
+// Thomas Phillips
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << " 1\t2\t3\t4\t5\t6\t7\t8\t9" << endl << "" << endl;
+  for (int c = 1; c < 10; c++) {
+    cout << c << "| ";
+    for (int i = 1; i < 10; i++) {
+      cout << i*c << '\t';
+    }
+    cout << endl;
+  }
+  return 0;
+}
---
+++ guess.cpp
@@ -0,0 +1,38 @@
+/*
+Time for us to start writing computer games! This first one will be a bit simple, but it's a start. Our task is to write a program that implements a guessing game. Our program generates a random number between 0 and 100. The player must guess the secret number. The program provides hints like "that's too high" or "that's too low" until the player finally guesses the secret number.
+*/
+
+// Thomas Phillips
+// June 21, 2017
+
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int main() {
+  const int CEILING = 100;
+  srand (time(NULL));
+
+  int randomNumber = rand() % CEILING + 1;
+  int guess;
+
+  do {
+    cout << "Guess a number: ";
+    while (!(cin >> guess) || guess < 0) {
+      cerr << "Bad input." << endl;
+      cin.clear();
+      cin.ignore(INT_MAX, '\n');
+      cout << "Guess a number: ";
+    }
+
+    if (guess < randomNumber) {
+      cout << "Too low." << endl;
+    } else if (guess > randomNumber) {
+      cout << "Too high." << endl;
+    }
+  } while (guess != randomNumber);
+
+  cout << "That's it!" << endl;
+  return 0;
+}
